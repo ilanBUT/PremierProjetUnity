e2b125e7b459538ffe2c32f6944cf1c4e7e39312
Ilan.Bazonnet <ilan.bazonnet@gmail.com>
2026-02-20 13:35:31 +0100

    Mise à jour du gestionnaire de pause

--- Assets/Scripts/pauseManager.cs
+++ Assets/Scripts/pauseManager.cs
@@ -4,4 +4,29 @@ public class poseManager : MonoBehaviour
 {
     private bool isGamePaused = false;
     // Update is called once per frame
+void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGamePaused = !isGamePaused;
+
+            if (isGamePaused == true)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+    private void Resume()
+    {
+        Time.timeScale = 1f;
+    }
 }
